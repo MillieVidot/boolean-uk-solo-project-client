15149f2bcb80ce26a8ddb52368b300cbea989af4
Camilla Vidot <camillavidot@gmail.com>
2021-10-01 11:30:48 +0100

    test

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import hero1 from "../assets/hero1.jpg"
+import trustpilot from "../assets/trustpilot.png"
 // const hero1 = require("../assets/hero1")
 
 export default function HomePage() {
   return (
     <div className="homepage">
       <img className="hero" src={hero1} alt="hero" />
-      <p>Save today with this package</p>
+      {/* <h2>Save today with this package</h2> */}
+      <img className="trust" src={trustpilot} alt="hero" />
     </div>
   )
 }
